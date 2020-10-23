9fc1d0cd454fb24fa701e955e5b4321d12dd858c
Tayu0404 <tkt.yusei0404@gmail.com>
2020-10-23 20:37:44 +0900

    first commit

---
+++ Apps-Install.bat
@@ -0,0 +1,40 @@
+choco install firefox-nightly --pre
+
+choco install keepassxc ^
+    7zip ^
+    audacity ^
+    cygwin ^
+    everything ^
+    ffmpeg ^
+    firefox ^
+    gimp ^
+    googlechrome ^
+    owncloud-client ^
+    qbittorrent ^
+    rufus ^
+    spotify ^
+    vscode ^
+    wget ^
+    winscp ^
+    youtube-dl ^
+    mpc-hc ^
+    discord ^
+    bzeditor ^
+    deepl ^
+    powertoys ^
+    pandoc ^
+    wireshark ^
+    virtualbox ^
+    microsoft-windows-terminal ^
+    slack ^
+    sublimetext3 ^
+    steam ^
+    origin ^
+    epicgameslauncher ^
+    procmon ^
+    cpu-z ^
+    foobar2000 ^
+    hwinfo ^
+    vcxsrv ^
+    hwmonitor ^
+    obs-studio 
