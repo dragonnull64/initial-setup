d3e286f3f7f8758ea7aacb35c960d359445b7585
Tayu0404 <tkt.yusei0404@gmail.com>
2020-10-26 21:35:46 +0900

    Update

--- Apps-Install.bat
+++ Apps-Install.bat
@@ -3,38 +3,43 @@ choco install firefox-nightly --pre --params "l=en-US"
 choco install keepassxc ^
     7zip ^
     audacity ^
+    audacity-lame ^
+    bzeditor ^
+    cpu-z ^
     cygwin ^
+    deepl ^
+    discord ^
+    epicgameslauncher ^
     everything ^
     ffmpeg ^
     firefox ^
+    flac ^
+    foobar2000 ^
     gimp ^
     googlechrome ^
+    googledrive ^
+    gpu-z ^
+    hwinfo ^
+    hwmonitor ^
+    microsoft-windows-terminal ^
+    mpc-hc ^
+    obs-studio ^
+    origin ^
     owncloud-client ^
+    pandoc ^
+    powertoys ^
+    powertoys ^
+    procmon ^
     qbittorrent ^
     rufus ^
+    slack ^
     spotify ^
+    steam ^
+    sublimetext3 ^
+    vcxsrv ^
+    virtualbox ^
     vscode ^
     wget ^
     winscp ^
-    youtube-dl ^
-    mpc-hc ^
-    discord ^
-    bzeditor ^
-    deepl ^
-    powertoys ^
-    pandoc ^
     wireshark ^
-    virtualbox ^
-    microsoft-windows-terminal ^
-    slack ^
-    sublimetext3 ^
-    steam ^
-    origin ^
-    epicgameslauncher ^
-    procmon ^
-    cpu-z ^
-    foobar2000 ^
-    hwinfo ^
-    vcxsrv ^
-    hwmonitor ^
-    obs-studio 
+    youtube-dl
