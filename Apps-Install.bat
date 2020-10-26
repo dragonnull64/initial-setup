d14721ce3579a0dafc4577dc1fdcb2ee470189ec
Tayu0404 <tkt.yusei0404@gmail.com>
2020-10-26 22:39:51 +0900

    Change so that you don't have to run it as an administrator

--- Apps-Install.bat
+++ Apps-Install.bat
@@ -1,46 +1,47 @@
-choco install firefox-nightly --pre --params "l=en-US"
-
-choco install keepassxc ^
-    7zip ^
-    audacity ^
-    audacity-lame ^
-    blender
-    bzeditor ^
-    cpu-z ^
-    cygwin ^
-    deepl ^
-    discord ^
-    epicgameslauncher ^
-    everything ^
-    ffmpeg ^
-    firefox ^
-    flac ^
-    foobar2000 ^
-    gimp ^
-    googlechrome ^
-    googledrive ^
-    gpu-z ^
-    hwinfo ^
-    hwmonitor ^
-    microsoft-windows-terminal ^
-    mpc-hc ^
-    obs-studio ^
-    origin ^
-    owncloud-client ^
-    pandoc ^
-    powertoys ^
-    procmon ^
-    qbittorrent ^
-    rufus ^
-    slack ^
-    spotify ^
-    steam ^
-    sublimetext3 ^
-    vcxsrv ^
-    virtualbox ^
-    visualstudio2019community
-    vscode ^
-    wget ^
-    winscp ^
-    wireshark ^
-    youtube-dl 
+@"%SystemRoot%\System32\cmd.exe" /C ^
+    "choco install -y firefox-nightly --pre --params "l=en-US" & ^
+    choco install -y ^
+        7zip ^
+        audacity ^
+        audacity-lame ^
+        blender ^
+        bzeditor ^
+        cpu-z ^
+        cygwin ^
+        deepl ^
+        discord ^
+        epicgameslauncher ^
+        everything ^
+        ffmpeg ^
+        firefox ^
+        flac ^
+        foobar2000 ^
+        gimp ^
+        googlechrome ^
+        googledrive ^
+        gpu-z ^
+        hwinfo ^
+        hwmonitor ^
+        keepassxc ^
+        microsoft-windows-terminal ^
+        mpc-hc ^
+        obs-studio ^
+        origin ^
+        owncloud-client ^
+        pandoc ^
+        powertoys ^
+        procmon ^
+        qbittorrent ^
+        rufus ^
+        slack ^
+        spotify ^
+        steam ^
+        sublimetext3 ^
+        vcxsrv ^
+        virtualbox ^
+        visualstudio2019community
+        vscode ^
+        wget ^
+        winscp ^
+        wireshark ^
+        youtube-dl"
---
+++ README.md
@@ -0,0 +1,53 @@
+# Initial-Setup
+Set up a Windows environment with Chocolatey
+
+## How to use
+1. Run `Chocolatey-Setup.bat`
+2. Run `Apps-Install.bat`
+
+## Package List
+* 7zip 
+* audacity 
+* audacity-lame 
+* blender
+* bzeditor 
+* cpu-z 
+* cygwin 
+* deepl 
+* discord 
+* epicgameslauncher 
+* everything 
+* ffmpeg 
+* firefox
+* firefox-nightly
+* flac 
+* foobar2000 
+* gimp 
+* googlechrome 
+* googledrive 
+* gpu-z 
+* hwinfo 
+* hwmonitor 
+* keepassxc 
+* microsoft-windows-terminal 
+* mpc-hc 
+* obs-studio 
+* origin 
+* owncloud-client 
+* pandoc 
+* powertoys 
+* procmon 
+* qbittorrent 
+* rufus 
+* slack 
+* spotify 
+* steam 
+* sublimetext3 
+* vcxsrv 
+* virtualbox 
+* visualstudio2019community
+* vscode 
+* wget 
+* winscp 
+* wireshark 
+* youtube-dl"
