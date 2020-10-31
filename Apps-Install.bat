78997026dfca38497ebecb952b2bc00cda056e47
Tayu0404 <tkt.yusei0404@gmail.com>
2020-10-31 18:36:38 +0900

    Add Thunderbird

--- Apps-Install.bat
+++ Apps-Install.bat
@@ -1,47 +1,48 @@
-@"%SystemRoot%\System32\cmd.exe" /C ^
-    "choco install -y firefox-nightly --pre --params "l=en-US" & ^
-    choco install -y ^
-        7zip ^
-        audacity ^
-        audacity-lame ^
-        blender ^
-        bzeditor ^
-        cpu-z ^
-        cygwin ^
-        deepl ^
-        discord ^
-        epicgameslauncher ^
-        everything ^
-        ffmpeg ^
-        firefox ^
-        flac ^
-        foobar2000 ^
-        gimp ^
-        googlechrome ^
-        googledrive ^
-        gpu-z ^
-        hwinfo ^
-        hwmonitor ^
-        keepassxc ^
-        microsoft-windows-terminal ^
-        mpc-hc ^
-        obs-studio ^
-        origin ^
-        owncloud-client ^
-        pandoc ^
-        powertoys ^
-        procmon ^
-        qbittorrent ^
-        rufus ^
-        slack ^
-        spotify ^
-        steam ^
-        sublimetext3 ^
-        vcxsrv ^
-        virtualbox ^
-        visualstudio2019community
-        vscode ^
-        wget ^
-        winscp ^
-        wireshark ^
-        youtube-dl"
+choco install -y firefox-nightly --pre --params "l=en-US" & ^
+choco install -y ^
+    7zip ^
+    audacity ^
+    audacity-lame ^
+    blender ^
+    bzeditor ^
+    cpu-z ^
+    cygwin ^
+    deepl ^
+    discord ^
+    epicgameslauncher ^
+    everything ^
+    ffmpeg ^
+    firefox ^
+    flac ^
+    foobar2000 ^
+    gimp ^
+    googlechrome ^
+    googledrive ^
+    gpu-z ^
+    hwinfo ^
+    hwmonitor ^
+    keepassxc ^
+    microsoft-windows-terminal ^
+    mpc-hc ^
+    obs-studio ^
+    origin ^
+    owncloud-client ^
+    pandoc ^
+    powertoys ^
+    procmon ^
+    qbittorrent ^
+    rufus ^
+    slack ^
+    spotify ^
+    steam ^
+    sublimetext3 ^
+    thunderbird ^
+    vcxsrv ^
+    virtualbox ^
+    visualstudio2019community
+    vscode ^
+    wget ^
+    winscp ^
+    wireshark ^
+    wsl2 ^
+    youtube-dl"
